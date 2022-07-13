147d6508809c9a4323d4e0c1d2c0662b6b63bd88
Andres Sumihe <672018136@student.uksw.edu>
2022-07-13 14:21:10 +0700

    Added some codes

---
+++ pages/components/footer/index.tsx
@@ -0,0 +1,14 @@
+import { FaBeer, FaHeart } from 'react-icons/fa'
+import variabels from '../../../styles/variables.module.scss';
+
+const Footer = (props: any) => {
+    const styles = props.styles;
+    console.log(variabels.emerald);
+    return <footer className={styles.footer}>
+        <div>
+            <p className='flex flex-row items-center font-semibold'> Made with <span className='mx-2'><FaHeart color={variabels.emerald} /></span> by Andres Sumihe</p>
+        </div>
+    </footer>
+}
+
+export default Footer;
---
+++ pages/components/hero.tsx
@@ -0,0 +1,11 @@
+import type {NextPage}  from "next";
+
+const Hero: NextPage = () => {
+
+    return (
+        <h1>
+            
+        </h1>
+    )
+
+}

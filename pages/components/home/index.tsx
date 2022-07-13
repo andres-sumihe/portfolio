ef342fbab900db7fdb0f4384e614834c12461fa6
Andres Sumihe <672018136@student.uksw.edu>
2022-07-13 23:38:53 +0700

    home section finished

--- pages/components/home/index.tsx
+++ pages/components/home/index.tsx
@@ -4,14 +4,14 @@ import variables from '../../../styles/_variables.module.scss';
 export default function Home(){
 
     return (
-        <section id="home" className="flex items-center px-48">
-            <div className="welcome-message">
+        <section id="home" className="flex flex-1 items-center justify-center px-10 md:px-36 lg:px-48">
+            <div className="welcome-message mr-auto">
                 <h1 className="welcome-title">Hello.</h1>
                 <h3 className="welcome-content">{"I'm"} <span className="name">Andres Sumihe</span></h3>
                 <h4 className="welcome-description">Mobile Develope - Web Developer</h4>
             </div>
 
-            <div className="absolute bottom-0 left-1/2 mx-auto mb-10">
+            <div className="absolute bottom-0 mb-10">
                 <CgScrollV className="heartbeat" size={48} color={variables.aliceBlue} />
             </div>
 

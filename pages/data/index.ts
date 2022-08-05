b2c5ff46eae57f2d5da744a1b441249e55940bb8
Andres Sumihe <672018136@student.uksw.edu>
2022-08-05 21:07:34 +0700

    added constant data

---
+++ pages/data/index.ts
@@ -0,0 +1,100 @@
+const softSkills = [
+    {
+        name: 'Leadership',
+        percent: 80
+    },
+    {
+        name: 'Teamwork',
+        percent: 85
+    },
+    {
+        name: 'Communication',
+        percent: 90
+    },
+    {
+        name: 'Problem Solving',
+        percent: 90
+    },
+]
+const hardSkills = [
+    {
+        name: 'HTML5 / CSS3',
+        percent: 85
+    },
+    {
+        name: 'JAVASCRIPT / REACT.JS',
+        percent: 80
+    },
+    {
+        name: 'PHP / LARAVEL',
+        percent: 50
+    },
+    {
+        name: 'DART / FLUTTER',
+        percent: 70
+    },
+]
+
+const experiences = [
+    {
+        jobTitle: 'Business Intelligence Intern',
+        date:'Jun 2022 - Present',
+        organization: 'PT Sumber Alfaria Trijaya, Tbk. Tangerang',
+        desc: [
+            `Develop several Forms for Human Resource Application using flask and webix.`,
+            `Work closely with other developers to debug and testing form.`,
+            `Using Google Cloud Platform to deploy application.`
+        ]
+    },
+    {
+        jobTitle: 'Quality Assurance Analyst Intern',
+        date:'Jan 2022 - Jun 2022',
+        organization: 'PT Sumber Alfaria Trijaya, Tbk. Tangerang',
+        desc: [
+            `Working on corporate scale application migration from VB.NET to Flask (Python based) as IT Quality Assurance.`,
+            `Developing test plans, test cases, test scripts, and test reports on Alfamart Property and Development System (PDS) Application and Building Maintenance Technical Support Application.`,
+            `Make a test result report.`
+        ]
+    },
+    {
+        jobTitle: 'Mobile Developer',
+        date:'Jul 2021 - Present',
+        organization: 'PT. Skala Teknologi Nusantara, Jakarta',
+        desc: [
+            `Develop Patient lifestyle tracker and monitoring for Pusat Jantung Nasional Harapan Kita (Harkit I-Care).`,
+            `Develop Harkit I-Care app using Flutter, Ghost API and Firebase.`,
+            `Manage testing, bugs fixing, and deployment to playstore.`
+        ]
+    },
+    {
+        jobTitle: 'Mobile Developer Intern',
+        date:'Jul 2019 - Present',
+        organization: 'Arisan Security Salatiga',
+        desc: [
+            `Contributing as an active member; write and review blog article for blog.arisansecurity.id.`,
+            `Working on web (arisansecurity.id) migration from Wordpress to Gatsby.js (since 2021).`,
+            `Manage testing, bugs fixing, and deployment to netlify.`
+        ]
+    },
+    {
+        jobTitle: 'Mobile Developer Intern',
+        date:'Sep 2021 - Des 2021',
+        organization: 'Marikh Salatiga',
+        desc: [
+            `Develop TaniPintar application. application of agricultural land allocation to help farmers manage land and make it easier for farmers to obtain information related to commodities and harvest amounts from other farmers in a farmer group.`,
+            'Build API Service for TaniPintar using Next.js (Serveless) and deploy to Oracle Cloud.',
+            'Manage testing, bugs fixing, and deployment to playstore.'
+        ]
+    },
+    
+]
+
+const educations = [
+    {
+        title: 'Bachelor of Computer',
+        date:'Aug 2018 - Oct 2022',
+        university: 'Universitas Kristen Satya Wacana',
+        gpa: '3.7'
+    }
+]
+export default {softSkills, hardSkills, experiences, educations}

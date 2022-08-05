8c3b7f082051637cf7f422db8544a91a86a9b377
Andres Sumihe <672018136@student.uksw.edu>
2022-08-05 21:07:51 +0700

    added poppins font

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import About from './components/about'
 import Footer from './components/footer'
 import Header from './components/header'
 import Home from './components/home'
+import Resume from './components/resume'
 
 const Main: NextPage = () => {
   return (
@@ -11,14 +12,16 @@ const Main: NextPage = () => {
       <Head>
         <title>Portfolio - Andres Sumihe</title>
         <meta name="description" content="Portfolio - Andres Sumihe" />
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,700,800&amp;display=swap" rel="stylesheet" />
       </Head>
       <Header />
 
       <main>
         <Home />
         <About />
+        <Resume />
       </main>
       <Footer/>
     </>
